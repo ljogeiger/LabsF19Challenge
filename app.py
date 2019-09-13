421a3f126529bb9076a20a21de288a5998d35be0
Lukas Geiger <geiger.ljo@gmail.com>
2019-09-12 23:44:11 -0400

    Implemented initial site. Gets JSON object. TODO: Parse data and display and documentation.

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, flash, url_for, session
+import requests
+import sys
 
 app = Flask(__name__)
+
+app.secret_key = '772f2253fd3a4c2524a93c70aefeac2e'
+
+base_url = "https://density.adicu.com/"
+token = ""
 
 
 @app.route('/', methods=['GET'])
@@ -8,5 +15,58 @@ def main():
     return render_template('index.html')
 
 
+@app.route('/getAccessToken', methods=['GET'])
+def getAccessToken():
+    return redirect('https://density.adicu.com/auth')
+
+
+@app.route('/enterToken', methods=['GET', 'POST'])
+def enterToken():
+    if request.method == 'POST':
+        token = request.form['text']
+        if token == "":
+            flash("You must enter a token.", 'danger')
+            return render_template('token.html')
+        else:
+            session['token'] = token
+            flash('Entered access token: ' +
+                  str(session.get('token', 'not set')), 'success')
+            return redirect(url_for('getData'))
+    elif request.method == 'GET':
+        return render_template('token.html')
+
+
+@app.route('/getData', methods=['GET', 'POST'])
+def getData():
+    token = session.get('token', 'not set')
+    if request.method == 'POST':
+        building = request.form['text'].strip().title()
+        number = request.form['number'].strip()
+        flash("" + building + str(type(number)), 'success')
+        if number is "":
+            json = requests.get(base_url + "latest/building/" +
+                                building + "?auth_token=" + token)
+            flash(base_url + "latest/building/" +
+                  building + "?auth_token=" + token, 'success')
+        else:
+            json = requests.get(base_url + "latest/building/" +
+                                number + "?auth_token=" + token)
+        return render_template('data.html')
+    elif request.method == 'GET':
+        return render_template('data.html')
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host='localhost', debug="true")
+
+
+"""
+Basic outline:
+    1 - Go to index.html
+    2 - Get Access Token (by redirecting user to ADI website)
+    3 - Get access token from user?
+    4 - Store and insert token
+    5 - Have user select building
+    6 - Make API call
+    7 - Parse and dislpay API JSON file
+"""
